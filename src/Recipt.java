75bf10c4dcb54658a61a9d7f92d8cd58b942d33b
Suraj Salgar <suraj935911@gmail.com>
2026-04-01 12:25:55 +0530

    Fisrt commit - Recipt project

---
+++ src/Recipt.java
@@ -0,0 +1,17 @@
+
+
+public class Recipt { public static void main(String[] args) {
+	String item = "Coffee";
+	int quantity = 2;
+	double price = 3.50;
+	double total = (quantity * price);
+	
+	System.out.println("I want to Drink " + item);
+	System.out.println("I want " +quantity+ " coffee");
+	System.out.println("This is the price of one coffee "+ price);
+	System.out.println("total price of cofee " + total);
+}
+}
+	
+	
+
